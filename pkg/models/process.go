68dd7d024425cb9b9b0e77ae4b87dd3aa79d36d8
Wycer <wycers@gmail.com>
2021-01-13 15:43:49 +0800

    🗑 clean

--- pkg/models/process.go
+++ pkg/models/process.go
@@ -8,23 +8,3 @@ type Process struct {
 
 	Definition string `json:"definition"`
 }
-
-//publicSpace, err := uuid.NewRandom()
-//if err != nil {
-//	return nil, errors.Wrap(err, "generate public space failed")
-//}
-//privateSpace, err := uuid.NewRandom()
-//if err != nil {
-//	return nil, errors.Wrap(err, "generate private space failed")
-//}
-//
-//if err := s.FileService.CreateFileSpace(publicSpace.String()); err != nil {
-//	return nil, err
-//}
-//if err := s.FileService.CreateFileSpace(privateSpace.String()); err != nil {
-//	return nil, err
-//}
-//
-//if p, err = s.SubmissionRepository.CreateProblem(title, locale, publicSpace.String(), privateSpace.String()); err != nil {
-//	return nil, err
-//}
