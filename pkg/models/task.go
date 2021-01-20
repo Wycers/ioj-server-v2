9f6559ad4848ecc74436a8fd1aa9e975902a76f3
Wycer <wycers@gmail.com>
2021-01-21 07:02:34 +0800

    💩 x

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -1,14 +1,19 @@
 package models
 
+type SlotDescriptor struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
 type Task struct {
 	Model
 
+	TaskId      string `json:"taskId"`
 	JudgementId string `json:"judgementId"`
-
-	TaskId string `json:"taskId"`
 
 	Type       string `json:"type"`
 	Properties string `json:"properties"`
-	Inputs     string `json:"inputs"`
-	Outputs    string `json:"outputs"`
+
+	Inputs  string `json:"inputs"`
+	Outputs string `json:"outputs"`
 }
