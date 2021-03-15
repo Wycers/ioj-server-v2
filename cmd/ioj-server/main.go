7975e5db2ab042639d6e763e437d47bdeb8efed7
wycers <wycers@gmail.com>
2021-03-15 14:33:27 +0800

    🚧 swagger

--- cmd/ioj-server/main.go
+++ cmd/ioj-server/main.go
@@ -6,6 +6,17 @@ import (
 
 var configFile = flag.String("f", "configs/server.yaml", "set config file which viper will loading.")
 
+
+// @title ioj-server
+// @version 0.0.2
+// @description API server for infinity-oj
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @schemes http https
+// @basePath /api/v1
+// @contact.name Wycer
+// @contact.email wycers@gmail.com
 func main() {
 	flag.Parse()
 
