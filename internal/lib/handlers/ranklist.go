a62783b68436e6ca668f29b33718c01802a15626
Wycer <wycers@gmail.com>
2021-10-24 04:12:59 +0800

    ✨ build-in function for ranklist update

--- internal/lib/handlers/ranklist.go
+++ internal/lib/handlers/ranklist.go
@@ -1,8 +1,17 @@
 package handlers
 
-import "github.com/infinity-oj/server-v2/pkg/models"
+import (
+	"github.com/infinity-oj/server-v2/internal/app/accounts"
+
+	"github.com/infinity-oj/server-v2/internal/app/ranklists"
+	"github.com/infinity-oj/server-v2/pkg/models"
+	"github.com/pkg/errors"
+	"github.com/spf13/cast"
+)
 
 type RankList struct {
+	rr ranklists.Repository
+	ar accounts.Repository
 }
 
 func (r RankList) IsMatched(tp string) bool {
@@ -10,9 +19,49 @@ func (r RankList) IsMatched(tp string) bool {
 }
 
 func (r RankList) Work(process *models.Process) error {
-	panic("implement me")
+	iRankListID, ok := process.Properties["ranklistID"]
+	if !ok {
+		return errors.New("ranklist id is not found")
+	}
+	rankListID := cast.ToUint64(iRankListID)
+	rl, err := r.rr.GetRankList(rankListID)
+	if err != nil {
+		return err
+	}
+	if rl == nil {
+		return errors.New("ranklist is not found")
+	}
+
+	iAccountID, ok := process.Properties["accountID"]
+	if !ok {
+		return errors.New("account id is not found")
+	}
+	accountID := cast.ToUint64(iAccountID)
+
+	account, err := r.ar.GetAccountById(accountID)
+	if err != nil {
+		return err
+	}
+	if rl == nil {
+		return errors.New("account is not found")
+	}
+
+	iKey, ok := process.Properties["metric_key"]
+	if !ok {
+		return errors.New("key is not found")
+	}
+	key := cast.ToString(iKey)
+
+	value := process.Inputs[0].Value
+
+	_, err = r.rr.CreateRankListRecord(rl, account, key, value)
+
+	return err
 }
 
-func NewRankList() *RankList {
-	return &RankList{}
+func NewRankList(rr ranklists.Repository, ar accounts.Repository) *RankList {
+	return &RankList{
+		rr: rr,
+		ar: ar,
+	}
 }
